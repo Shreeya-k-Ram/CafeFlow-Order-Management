83b35ce202ac389318e729d44a06f818b578bbb1
Shreeya Kumari Ram <shreeya.kumari.ram2736@gmail.com>
2026-06-17 12:59:27 +0530

    Added customer order

--- cafe_project/cafe/models.py
+++ cafe_project/cafe/models.py
@@ -8,5 +8,26 @@ class MenuItem(models.Model):
     price = models.DecimalField(max_digits=8, decimal_places=2)
     available = models.BooleanField(default=True)
 
+class Order(models.Model):
+    customer_name = models.CharField(max_length=100)
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    STATUS_CHOICES = [
+        ("Pending", "Pending"),
+        ("Preparing", "Preparing"),
+        ("Completed", "Completed"),
+    ]
+
+    status = models.CharField(
+        max_length=20,
+        choices=STATUS_CHOICES,
+        default="Pending"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.name
+
+    def __str__(self):
+        return f"{self.customer_name} - {self.menu_item.name}"
