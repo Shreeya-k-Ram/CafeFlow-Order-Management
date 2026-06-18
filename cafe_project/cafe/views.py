0146851b6a8f1dfb30de495f489023ae3b3f2800
Shreeya Kumari Ram <shreeya.kumari.ram2736@gmail.com>
2026-06-18 15:17:51 +0530

    Added views, templates and urls

--- cafe_project/cafe/views.py
+++ cafe_project/cafe/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import MenuItem
 
 # Create your views here.
+def menu_list(request):
+    menu_items = MenuItem.objects.all()
+
+    return render(
+        request,
+        'cafe/menu_list.html',
+        {'menu_items': menu_items}
+    )
+
